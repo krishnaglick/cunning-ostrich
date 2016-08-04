ca3a2b34bd525d4a081b88a270d7bb110e30f2f7
Krishna Glick <unstoppable411@gmail.com>
2016-08-04 11:03:54 -0400

    lol...

--- client/app/authorizers/application.js
+++ client/app/authorizers/application.js
@@ -5,7 +5,7 @@ import Base from 'ember-simple-auth/authorizers/base';
 export default Base.extend({
   session: Ember.inject.service(),
   authorize(session, setRequestHeader) {
-    if (this.get('session.isAuthenticated') && !Ember.isEmpty(session.token) && ~Ember.isEmpty(session.email)) {
+    if (this.get('session.isAuthenticated') && !Ember.isEmpty(session.token) && !Ember.isEmpty(session.email)) {
       setRequestHeader('token', session.token);
       setRequestHeader('email', session.email);
     }
